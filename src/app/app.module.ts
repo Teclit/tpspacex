826f725af4824a9b717b88c4b3e7094785278619
EKIA Benjamin <ekiabenjamin@gmail.com>
2022-11-23 17:06:59 +0100

    add component capsules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapsulesComponent } from './capsules/capsules.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapsulesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/capsules/capsules.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-capsules',
+  templateUrl: './capsules.component.html',
+  styleUrls: ['./capsules.component.css']
+})
+export class CapsulesComponent {
+
+}
