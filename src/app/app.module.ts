f8e0d199a32da70289db308b8d9c7d311e8140f4
Teklit <teklitte@gmail.com>
2022-11-23 16:58:42 +0100

    Create component Launches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from'@angular/common/http';
+import { LaunchesComponent } from './launches/launches.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LaunchesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
