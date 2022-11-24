3a9c728013323314e9f2e2fb718ab0b4d93cd1c8
Teklit <teklitte@gmail.com>
2022-11-24 09:26:52 +0100

    Card test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
-  title= "TP | Benjamin et Teklit"
+  title= "TP | SpaceX Launches"
   
 }
